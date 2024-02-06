bf73d47dab38793ad2635f2d43f2cf0d13d77776
hyeonnjii <114765085+hyeonnjii@users.noreply.github.com>
2024-02-07 01:42:57 +0900

    feat: Allow list and tuple arguments for combination function
    Add the feature to pass list and tuple as arguments to combination
    function.
    Add some examples to verify the function.

--- combination.py
+++ combination.py
@@ -1,21 +1,31 @@
 ### Combination ###
 
 # version1
-def combination(n, r):
+def combination(nums, r):
+    if isinstance(nums, int):
+        nums = [x for x in range(1, nums+1)]
+    elif not isinstance(nums, (list, tuple)):
+        raise "Inappropriate data structure! Only integer, iterable object(list, tuple but not string here)"
+
     ans = []
 
     def recur(start, tmp_ans):
         if len(tmp_ans) == r:
             ans.append(tmp_ans[:])
             return
-        for i in range(start, n+1):
-            tmp_ans.append(i)
+        for i in range(start, len(nums)):
+            tmp_ans.append(nums[i])
             recur(i+1, tmp_ans)
             tmp_ans.pop()
         
-    recur(1, [])
+    recur(0, [])
     return ans
 
 # Examples
 print(combination(4, 2))
 print(len(combination(7, 3)), combination(7, 3)) 
+print(combination([3, 5, 18, 9], 2))
+
+print(combination(1, 1))
+print(combination(2, 3))
+
